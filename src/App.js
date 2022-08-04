b14b1a0bab82336c00434d96037259348e51c5e8
Brutosippon <joaoccfidalgo@gmail.com>
2022-08-04 17:47:43 -0100

    heroku

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -22,5 +23,17 @@ function App() {
   );
 }
 
+/*const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static())
+}
+app.listen(port, (err) =>{
+  if (err) return console.log(err);
+  console.log('Server running on port: ', port)
+})*/ 
 
 export default App;
