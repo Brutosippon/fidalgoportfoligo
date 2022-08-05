ae0d5e4465fc08936dee236ddf2fcbe011f02e3f
Brutosippon <joaoccfidalgo@gmail.com>
2022-08-05 15:35:03 -0100

    heroku frist deploy

--- src/App.js
+++ src/App.js
@@ -23,17 +23,4 @@ function App() {
   );
 }
 
-/*const express = require('express');
-const app = express();
-const path = require('path');
-const port = process.env.PORT || 5000;
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static())
-}
-app.listen(port, (err) =>{
-  if (err) return console.log(err);
-  console.log('Server running on port: ', port)
-})*/ 
-
 export default App;
